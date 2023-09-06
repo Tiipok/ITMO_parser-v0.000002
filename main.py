7c54b398d967a066a7cfb4c69c9593d8774206ac
Tipok <60702813+Tiipok@users.noreply.github.com>
2023-09-06 17:20:18 +0300

    ui added

--- main.py
+++ main.py
@@ -1,45 +1,66 @@
 import requests  
 from datetime import datetime
 from ics import Calendar, Event
-from pprint import pprint
+from PyQt5 import QtWidgets, uic
+import sys
 
 
-def main():
-    START_DAY = '10' # input('input start day(01, 02, 11, 15...): ')
-    LAST_DAY = '20' # input('input last day(01, 02, 11, 15...): ')
-    MONTH = '09' # input('input month(01, 02, 11...): ')
-    YEAR = '2023'
-    TOKEN = "Bearer eyJhbGciOiJ..."
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__() 
+        uic.loadUi('mainWindow.ui', self)
+        self.addFunc() 
+        self.show() 
     
-    headers = {"Authorization": TOKEN}
-    url = f'https://my.itmo.ru/api/schedule/schedule/personal?date_start={YEAR}-{MONTH}-{START_DAY}&date_end={YEAR}-{MONTH}-{LAST_DAY}'
-    schedule = requests.get(url=url, headers=headers).json()['data']
+    def addFunc(self):
+        self.ImportButton.clicked.connect(lambda:self.ImportSchedule())
 
-    calendar = Calendar()
+    def ImportSchedule(self):
 
-    for day in schedule:
-        date = day['date']
-        for lesson in day['lessons']:
-            
-            try:
-                event = Event()
-                event.description = lesson['building']
-                event.name = lesson['subject']
-                event.begin = datetime.fromisoformat(f'{date}T{lesson["time_start"]}+03:00')
-                event.end = datetime.fromisoformat(f'{date}T{lesson["time_end"]}+03:00')
-            
-                calendar.events.add(event)
+        start_date = self.dateEdit_1.text().split('.')
+        end_date = self.dateEdit_2.text().split('.')
+        
+        START_DAY = start_date[0]
+        END_DAY = end_date[0]
 
-            except TypeError:
-                pprint(lesson)
-    
-    open('schedule.ics', 'w', encoding="utf-8").close()
+        START_MONTH = start_date[1]
+        END_MONTH = end_date[1]
 
-    with open("schedule.ics", "w", encoding="utf-8") as f:
-        f.write(calendar.serialize())
-    
-    print('Done')
-              
+        START_YEAR = start_date[2]
+        END_YEAR = end_date[2]
 
+        TOKEN = self.lineEdit.text()
+        
+        headers = {"Authorization": TOKEN}
+        url = f'https://my.itmo.ru/api/schedule/schedule/personal?date_start={START_YEAR}-{START_MONTH}-{START_DAY}&date_end={END_YEAR}-{END_MONTH}-{END_DAY}'
+
+        schedule = requests.get(url=url, headers=headers).json()['data']
+
+        calendar = Calendar()
+
+        try:
+            for i, day in enumerate(schedule):
+                self.label_4.setText(f'{i+1}/{len(schedule)}')
+                date = day['date']
+                for lesson in day['lessons']:
+                    
+                    event = Event()
+                    event.description = lesson['building']
+                    event.name = lesson['subject']
+                    event.begin = datetime.fromisoformat(f'{date}T{lesson["time_start"]}+03:00')
+                    event.end = datetime.fromisoformat(f'{date}T{lesson["time_end"]}+03:00')
+                    
+                    calendar.events.add(event)
+
+                    with open(f"{self.dateEdit_1.text()}-{self.dateEdit_2.text()}.ics", "w", encoding="utf-8") as f:
+                        f.write(calendar.serialize())
+                        self.label_4.setText('Done')
+
+        except TypeError:
+            self.label_4.setText('error, perhaps your\ntoken is invalid')
+        
+        
 if __name__ == '__main__':
-    main()
+    app = QtWidgets.QApplication(sys.argv) 
+    window = Ui() 
+    app.exec_()
